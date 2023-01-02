bf6dedb8b306c57dba80fe7929146faae3eea64c
Bruno Gisse <brunogisseju@outlook.com>
2023-01-02 14:37:35 -0300

    DTO

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/CategoryRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/CategoryRepository.java
@@ -1,10 +1,8 @@
 package com.devsuperior.dscatalog.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.devsuperior.dscatalog.entities.Category;
-
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.services.CategoryService;
 
 
@@ -21,9 +21,9 @@ public class CategoryResource {
 	@Autowired // Spring will inject a valid dependency from this repository
 	private CategoryService service;
 	
-	@GetMapping //setting that the method will be a web service (an end point from the resource from Category). 
-	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = service.findAll();		
+	@GetMapping //setting that the method will be a web service (an end point from the resource from CategoryDTO). 
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<CategoryDTO> list = service.findAll();		
 		return ResponseEntity.ok().body(list);
 	}
 
